71bcddd67a90545f94d8bbfa5e69829142fec732
alialaba <alialaba079@gmail.com>
2025-01-13 15:38:21 -0500

    feat: implement lazy loading imgs

--- script/index.js
+++ script/index.js
@@ -126,3 +126,30 @@ allSection.forEach(section=>{
     sectionObserver.observe(section);
     section.classList.add("section-hidden");  
 })
+
+
+//Lazy loading image
+const imgTargets = document.querySelectorAll("img[data-src]");
+console.log(imgTargets)
+
+const loadingImg =  function(entries, observer){
+ const [entry] = entries;
+ console.log(entry)
+ if(!entry.isIntersecting) return;
+
+ //Replace image with data-src
+ entry.target.src = entry.target.dataset.src;
+ 
+
+ entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img")
+ })
+ observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadingImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: "200px"
+})
+imgTargets.forEach(img=> imgObserver.observe(img) )
